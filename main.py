c87c946a3c5f8aec75d37cea62df345cc35206fc
qmt8843 <qmt8843@rit.edu>
2024-11-25 12:42:13 -0500

    Created main.py with very basic concept blocking

---
+++ main.py
@@ -0,0 +1,13 @@
+#Ask for/import domain
+
+#Search indexers for URLs associated with that domain
+# Google, bing, duckduckgo, yandex
+
+#Download files found at URLs
+# Tag format: [indexer]-[datetime]-[url]-[filename]
+
+#Based on file type, search for metadata
+# html searches for comments
+
+#Analyize data
+# What does this entail??
